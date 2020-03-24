afef2f97d4d10a529ae2e24f176029805c108922
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-03-24 17:23:42 +0300

    Added slider element

---
+++ source/js/script.js
@@ -0,0 +1,10 @@
+$('.slider-header').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  arrows: true,
+  dots: true,
+  prevArrow: '.slider__controls-prev',
+  nextArrow: '.slider__controls-next',
+});

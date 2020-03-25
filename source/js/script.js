9415517aaef57534f68a47b9623f71290e91ad5e
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-03-25 12:07:47 +0300

    Added second header slider

--- source/js/script.js
+++ source/js/script.js
@@ -9,13 +9,13 @@ $('.slider-header').slick({
   nextArrow: '.slider__controls-next',
 });
 
-$('header--wholesalers .heder.slider-header').slick({
+$('.slider-wholesalers').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: false,
   autoplaySpeed: 3000,
   arrows: true,
-  dots: true,
-  prevArrow: '.slider__controls-prev',
-  nextArrow: '.slider__controls-next',
+  dots: false,
+  prevArrow: '.header--wholesalers .slider__controls-prev',
+  nextArrow: '.header--wholesalers .slider__controls-next',
 });
